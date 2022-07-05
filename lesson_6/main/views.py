90e3b01ebe12e6ab6184f1d745baf5c9837ac000
Kamoliddin0606 <35606751+Kamoliddin0606@users.noreply.github.com>
2022-07-05 19:52:17 +0500

    category filter

--- lesson_6/main/views.py
+++ lesson_6/main/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Product
 # Create your views here.
-def index(request):
+def index(request, id=None):
     objects = Category.objects.all()
-    print(objects)
-    context ={'objects':objects}
+
+    if  id==None:
+        products = Product.objects.all()
+
+    else:  
+        objectcat = Category.objects.get(id=id)
+        products = Product.objects.filter(category=objectcat)
+
+    print(products.query)
+    context ={'objects':objects, 'products':products}
     return render(request=request, template_name='main/index.html', context=context)
